8438abc56d34c02b3c42500b5fad2a5a55aa4bbf
Docartes <ahmadv0101@gmail.com>
2025-11-30 20:11:21 +0700

    ffix error and bug

--- index.js
+++ index.js
@@ -12,24 +12,63 @@ const lengthButton = document.querySelector('button#length-units')
 const weightButton = document.querySelector('button#weight-units')
 const tempButton = document.querySelector('button#temp-units')
 
+const lengthUnits = {
+	mm: 0.001, cm: 0.01, m: 1, km: 1000, inch: 0.0254, foot: 0.3048, yard: 0.9144, mile: 1609.344
+}
 
-function convertLength(value, from, to) {
-	const toMeters = {
-		mm: 0.001, cm: 0.01, m: 1, km: 1000, inch: 0.0254, foot: 0.3048, yard: 0.9144, mile: 1609.344,
-		mg: 0.01, g: 1, kg: 1000, ounce: 0.0352, pound: 0.0022 
-	}
+const weightUnits = {
+	mg: 0.01, g: 1, kg: 1000, ounce: 0.0352, pound: 0.0022 
+}
 
-	if ( !(from in toMeters) || !(to in toMeters) ) {
-		return resultOutput.innerHTML = `Invalid Unit. Supported: ${Object.keys(toMeters).join(', ')}`
+const tempUnits = {
+	celcius: 0, kelvin: 273, fahrenheit: 32
+}
+
+
+function convert(value, from, to, units) {
+	if ( !(from in units) || !(to in units) ) {
+		return resultOutput.innerHTML = `Invalid Unit. Supported: ${Object.keys(units).join(', ')}`
 	}
 
 	if ( typeof value !== 'number' || value < 0 ) {
 		return resultOutput.innerHTML = 'Value cannot be a negative or not a number'
 	}
 
-	const result = value * (toMeters[from] / toMeters[to])
-	Math.round(result, 6) 
+	const result = value * (units[from] / units[to])
+	result.toFixed(6) 
 	return resultOutput.innerHTML = `${value} ${from} = ${result} ${to}`
+}
+
+function convertTemp(value, from, to) {
+	if ( typeof value !== 'number' ) {
+		return resultOutput.innerHTML = 'Value cannot be not NaN'
+	}
+
+	let result = 0
+	let tempKelvin = 0
+
+	if ( from == 'celcius' || from == 'c' ) {
+		tempKelvin = value + 273.15
+	} else if ( from == 'fahrenheit' || from == 'f' ) {
+		tempKelvin = (value - 32) * 5/9 + 273.15
+	} else if ( from == 'kelvin' || from == 'k' ) {
+		tempKelvin = value
+	} else {
+		return resultOutput.innerHTML = 'Invalid from unit'
+	}
+
+	if ( to == 'celcius' || to == 'c' ) {
+		result = tempKelvin  - 273.15
+	} else if ( to == 'fahrenheit' || to == 'f' ) {
+		result = (tempKelvin - 273.15) * 9/5 + 32
+	} else if ( to == 'kelvin' || to == 'k' ) {
+		result = tempKelvin
+	} else {
+		return resultOutput.innerHTML = 'Invalid from unit'
+	}
+
+	return resultOutput.innerHTML = `${value} ${from} = ${result} ${to}`
+
 }
 
 function clearValue() {
@@ -44,7 +83,22 @@ function clearValue() {
 convertButton.addEventListener('click', () => {
 	inputArea.classList.add('hidden')
 	resultArea.classList.remove('hidden')
-	convertLength((inputLength.value !== null || inputWeight.value !== null), inputFrom.value, inputTo.value)
+	
+	if ( !(inputLength.classList.contains('hidden')) ) {
+		convert(Number(inputLength.value), inputFrom.value, inputTo.value, lengthUnits)
+		clearValue()
+	} 
+
+	if ( !(inputWeight.classList.contains('hidden')) ) {
+		convert(Number(inputWeight.value), inputFrom.value, inputTo.value, weightUnits)
+		clearValue()
+	}
+
+	if ( !(inputTemp.classList.contains('hidden')) ) {
+		convertTemp(Number(inputTemp.value), inputFrom.value, inputTo.value)
+		clearValue()
+	}
+
 	clearValue()
 })
 
@@ -54,24 +108,21 @@ resetButton.addEventListener('click', () => {
 	clearValue()
 })
 
-lengthButton.addEventListener('click', () => {
-	inputLength.classList.remove('hidden')
-	weight.classList.add('hidden')
-	inputTemp.classList.add('hidden')
+
+lengthButton.addEventListener('click', () => { 
+	inputLength.classList.remove('hidden') 
+	inputWeight.classList.add('hidden') 
+	inputTemp.classList.add('hidden') 
+}) 
+
+weightButton.addEventListener('click', () => { 
+	inputLength.classList.add('hidden') 
+	inputWeight.classList.remove('hidden') 
+	inputTemp.classList.add('hidden') 
+}) 
+
+tempButton.addEventListener('click', () => { 
+	inputLength.classList.add('hidden') 
+	inputTemp.classList.remove('hidden') 
+	inputWeight.classList.add('hidden') 
 })
-
-weightButton.addEventListener('click', () => {
-	inputLength.classList.add('hidden')
-	inputWeight.classList.remove('hidden')
-	inputTemp.classList.add('hidden')
-})
-
-tempButton.addEventListener('click', () => {
-	inputLength.classList.add('hidden')
-	inputTemp.classList.remove('hidden')
-	inputWeight.classList.add('hidden')
-})
-
-
-
-
